9ed62983a12325b7cf1c261ad186fe8c37890e8a
Michelle <mliuu3259@gmail.com>
2021-06-28 11:49:02 -0400

    changed import to require in build.js

--- feedback-module/build.js
+++ feedback-module/build.js
@@ -1,5 +1,4 @@
-import rewire from "rewire";
-// const rewire = require('rewire');
+const rewire = require("rewire");
 const defaults = rewire("react-scripts/scripts/build.js");
 const config = defaults.__get__("config");
 
