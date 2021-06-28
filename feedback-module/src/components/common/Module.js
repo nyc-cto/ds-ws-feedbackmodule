299627338f0e5c262cd4528e18d4c160d5bfdd96
Michelle <mliuu3259@gmail.com>
2021-06-28 13:46:25 -0400

    set up logic for switching between screens

--- feedback-module/src/components/common/Module.js
+++ feedback-module/src/components/common/Module.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { GridContainer, Grid } from "@trussworks/react-uswds";
 
 import Header from "./Header";
-import ModuleCheckbox from "./Checkbox";
-import ModuleButton from "./Button";
+import Screen1 from "../pages/Screen1";
+import Screen2 from "../pages/Screen2";
 
 function Module() {
+  const [screen, setScreen] = useState(
+    <Screen1
+      setData={(data) => setScreen(<Screen2 data={data} />)}
+      page="[this page]"
+    />
+  );
+
   return (
     <GridContainer
       desktop={{ col: 2 }}
@@ -13,16 +20,7 @@ function Module() {
       className="bg-primary-light radius-top-lg padding-x-0"
     >
       <Header />
-      <Grid className="padding-x-6 padding-y-5">
-        <ModuleCheckbox label="test" id="checkbox-test" />
-        <ModuleButton
-          onClick={() => {
-            alert("Feedback button has been pressed");
-          }}
-          style={{ cursor: "pointer" }}
-          buttonText="Button"
-        />
-      </Grid>
+      <Grid className="padding-x-6 padding-y-5">{screen}</Grid>
     </GridContainer>
   );
 }
