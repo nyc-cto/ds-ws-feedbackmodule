06581954358ad2577909fa17277444078ce8365b
Michelle <mliuu3259@gmail.com>
2021-06-29 16:15:24 -0400

    set the buttons from screen 1 to make a post request to the api to test integrating react w/ express— need to change later

--- feedback-module/src/components/pages/Screen1.js
+++ feedback-module/src/components/pages/Screen1.js
@@ -2,11 +2,20 @@ import React from "react";
 
 import ModuleButton from "../common/Button";
 import { SCREEN1_BUTTONS } from "../../assets/constants";
+import axios from "axios";
 
 function Screen1({ setData, page }) {
+  const getFeedback = (text) => {
+    axios
+      .post("/api/feedback", { feedback: text })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   const buttons = SCREEN1_BUTTONS.map(({ text, data }, index) => {
     const handleClick = (e) => {
       setData(data);
+      getFeedback(text);
       e.preventDefault();
     };
 
