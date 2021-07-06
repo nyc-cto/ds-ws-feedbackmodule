0ba207fbe6e52f7bbc9967dfaed3b3b9e5344958
Kara Siegel <ksiegel1923@gmail.com>
2021-07-06 15:07:32 -0400

    added translation file that links to keys in constants files

--- feedback-module/src/assets/constants.js
+++ feedback-module/src/assets/constants.js
@@ -22,76 +22,48 @@ export const SCREENS = {
     ],
   },
   missing_info: {
-    title:
-      "Sorry you couldn't find what you were looking for! Can you tell us a little more?",
-    checkboxes: [
-      "I wasn't sure where to look",
-      "I was confused by the information I found",
-      "I don't understand how to use this website",
-      "I hit a dead end",
-      "The content was not available in my preferred language",
-      "There were errors with the translation available",
-      "Other",
-    ],
-    textInputs: [
-      { type: "text", text: "What were you looking for or trying to do?" },
-      { type: "text", text: "Would you like to add anything else?" },
-    ],
+    title: "missingTitle",
+    checkboxes: "missingCheckboxes",
+    textInputs: "missingTextInputs",
     buttons: [
       { type: "submit", text: "submitBtn", nextScreen: "feedback_results" },
     ],
   },
   broken: {
-    title:
-      "Sorry to hear that something is broken or inaccurate! Can you tell us a little more?",
-    checkboxes: [
-      "There is a spelling mistake or typo",
-      "A link or button does not work",
-      "The information is out of date",
-      "I got different informatino from another source (311, printed mailer, elected official, etc.)",
-      "The layout looks broken",
-      "I got an error",
-      "Other",
-    ],
-    textInputs: [
-      "Can you give us more details on what you were trying to do, or the exact problem you encountered? (optional)",
-    ],
+    title: "brokenTitle",
+    checkboxes: "brokenCheckboxes",
+    textInputs: "brokenTextInputs",
     buttons: [
       { type: "submit", text: "submitBtn", nextScreen: "feedback_results" },
     ],
   },
   other: {
-    title: "We appreciate your feedback.",
-    textInputs: ["Can you tell us a little more?"],
+    title: "otherTitle",
+    textInputs: "otherTextInputs",
     buttons: [
       { type: "submit", text: "submitBtn", nextScreen: "feedback_results" },
     ],
   },
   feedback_results: {
-    titleInverse:
-      "Thank you for your feedback! It has been submitted.<br /><br />Your feedback is anonymous and confidential, so you will not receive a reply.",
-    plain_text:
-      "The City of New York is always trying to improve its services. Are you interesed in being a user research participant? This is is entirely voluntary and you can opt out at any time. Signing up to be a user research participant will have no impact on your feedback today, or your eligibility to access or receive services in the future.",
+    titleInverse: "feedbackResultsTitle",
+    plain_text: "feedbackResultsPlain_text",
     buttons: [
-      { type: "submit", text: "Yes, sign me up!", nextScreen: "user_research" },
+      {
+        type: "submit",
+        text: "feedbackResultsButton",
+        nextScreen: "user_research",
+      },
     ],
   },
   user_research: {
-    title:
-      "As a user research participant you can help us continue to improve services for New Yorkers",
-    plain_text:
-      "Please leave your contact details and we will get in touch with you. Being a research participant means you may be contacted from time to time to help us test new services and products. We will send you an email with more details.<br />You can opt out at any time.",
-    textInputs: [
-      { type: "text", text: "Your name" },
-      { type: "email", text: "Your email" },
-      { type: "tel", text: "Your phone number" },
-    ],
+    title: "userResearchTitle",
+    plain_text: "userResearchPlain_text",
+    textInputs: "userResearchTextInputs",
     buttons: [
       { type: "submit", text: "submitBtn", nextScreen: "user_feedback_result" },
     ],
   },
   user_feedback_result: {
-    titleInverse:
-      "Thank you for signing up to be a user research participant!  We’ll be in touch if there are any opportunities to test products and services.",
+    titleInverse: "userFeedbackResultsTitle",
   },
 };
--- feedback-module/src/components/pages/Screen1.js
+++ feedback-module/src/components/pages/Screen1.js
@@ -4,13 +4,15 @@ import { useTranslation } from "react-i18next";
 
 import ModuleButton from "../common/Button";
 import { SCREEN1_BUTTONS } from "../../assets/constants2";
+import { SCREENS } from "../../assets/constants";
 import { SCREEN_CONTAINER_STYLE } from "../../assets/styling_classnames";
+import TextboxList from "../TextboxList";
 
 function Screen1({ changePage, page, setFeedback }) {
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    i18n.changeLanguage("esp");
+    i18n.changeLanguage("en");
   }, []);
 
   const buttons = SCREEN1_BUTTONS.map(({ text, data }, index) => {
@@ -31,6 +33,13 @@ function Screen1({ changePage, page, setFeedback }) {
       <h1>Do you have any feedback on {page}?</h1>
       <p>{t("test")}</p>
       {buttons}
+      <ModuleButton buttonText={t(SCREENS.feedback_type.buttons[0].text)} />;
+      {console.log(t(SCREENS.broken.checkboxes, { returnObjects: true }))}
+      {console.log(t(SCREENS.missing_info.textInputs, { returnObjects: true }))}
+      <TextboxList
+        inputs={t(SCREENS.missing_info.textInputs, { returnObjects: true })}
+        size="area"
+      />
     </Grid>
   );
 }
