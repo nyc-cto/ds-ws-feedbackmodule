2ba4cb1af591fd96258ed84a4e279438092d244e
Kara Siegel <ksiegel1923@gmail.com>
2021-07-13 11:32:11 -0400

    Styled error component to match Figma designs and house styles

--- feedback-module/src/components/common/ErrorAlert.js
+++ feedback-module/src/components/common/ErrorAlert.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Alert } from "@trussworks/react-uswds";
+import FeatherIcon from "feather-icons-react";
 
 function ErrorAlert({ errorText }) {
   return (
-    <Alert type="error" slim>
-      {errorText}
+    <Alert type="error" slim noIcon>
+      <div className="display-flex">
+        <FeatherIcon
+          icon="alert-triangle"
+          size="22"
+          strokeWidth="2.5"
+          className="margin-right-205 padding-top-2px"
+        />
+        {errorText}
+      </div>
     </Alert>
   );
 }
