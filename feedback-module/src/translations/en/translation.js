90a20a02ab7463546b5908b7a14b46788ace3a04
Kara Siegel <ksiegel1923@gmail.com>
2021-07-19 11:44:41 -0400

    cleaned up / set up repository, moved translation files out of public, updated i18n file based on new location of translation files

---
+++ feedback-module/src/translations/en/translation.js
@@ -0,0 +1,95 @@
+export const TRANSLATIONS_EN = {
+  submitBtn: "Submit",
+
+  feedbackType: {
+    title: "Do you have any feedback on {{page}}?",
+  },
+
+  missing: {
+    initialBtn: "I couldn't find what I was looking for",
+    title:
+      "Sorry you couldn't find what you were looking for! Can you tell us a little more?",
+    checkboxes: [
+      "I wasn't sure where to look",
+      "I was confused by the information I found",
+      "I don't understand how to use this website",
+      "I hit a dead end",
+      "The content was not available in my preferred language",
+      "There were errors with the translation available",
+      "Other",
+    ],
+    textInputs: [
+      {
+        type: "textarea",
+        text: "What were you looking for or trying to do?",
+      },
+      { type: "textarea", text: "Would you like to add anything else?" },
+    ],
+  },
+
+  broken: {
+    initialBtn: "Something appears to be broken or inaccurate",
+    title:
+      "Sorry to hear that something is broken or inaccurate! Can you tell us a little more?",
+    checkboxes: [
+      "There is a spelling mistake or typo",
+      "A link or button does not work",
+      "The information is out of date",
+      "I got different information from another source (311, printed mailer, elected official, etc.)",
+      "The layout looks broken",
+      "I got an error",
+      "Other",
+    ],
+    textInputs: [
+      {
+        type: "textarea",
+        text:
+          "Can you give us more details on what you were trying to do, or the exact problem you encountered?",
+      },
+    ],
+  },
+
+  other: {
+    initialBtn: "I have other feedback on this page or website",
+    title: "We appreciate your feedback.",
+    textInputs: [
+      {
+        type: "textarea",
+        text: "Can you tell us a little more?",
+        required: true,
+      },
+    ],
+  },
+
+  feedbackResults: {
+    title:
+      "Thank you for your feedback! It has been submitted.<br /><br />Your feedback is anonymous and confidential, so you will not receive a reply.",
+    plainText:
+      "The City of New York is always trying to improve its services. Are you interesed in being a user research participant? This is is entirely voluntary and you can opt out at any time. Signing up to be a user research participant will have no impact on your feedback today, or your eligibility to access or receive services in the future.",
+    button: "Yes, sign me up!",
+  },
+
+  userResearch: {
+    title:
+      "As a user research participant you can help us continue to improve services for New Yorkers",
+    plainText:
+      "Please leave your contact details and we will get in touch with you. Being a research participant means you may be contacted from time to time to help us test new services and products. We will send you an email with more details.<br />You can opt out at any time.",
+    textInputs: [
+      { type: "text", text: "Your name", required: true },
+      { type: "email", text: "Your email", required: true },
+      { type: "tel", text: "Your phone number" },
+    ],
+  },
+
+  userFeedbackResults: {
+    title:
+      "Thank you for signing up to be a user research participant!  We’ll be in touch if there are any opportunities to test products and services.",
+  },
+
+  errorMessages: {
+    checkboxError: "Please select at least one option.",
+    nameError: "Please provide your name.",
+    emailPhoneError: "Please provide your email or phone number.",
+    inputEmptyError: "Please provide a response.",
+  },
+};
