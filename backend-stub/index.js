a011765b9f81cddbf31e4e11c26c87c89387c205
Michelle <mliuu3259@gmail.com>
2021-07-19 15:20:30 -0400

    set access-control-allow-origin

--- backend-stub/index.js
+++ backend-stub/index.js
@@ -10,13 +10,11 @@ app
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use((req, res, next) => {
-    app.use(function (req, res, next) {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-      res.setHeader("Access-Control-Allow-Credentials", true);
-      next();
-    });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
   });
 
 const apiCall = async (url, data) => {
