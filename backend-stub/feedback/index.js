3560ffe6058a265ec3d8b31733b6e76f3307251f
Michelle <mliuu3259@gmail.com>
2021-07-20 11:07:58 -0400

    return request body

--- backend-stub/feedback/index.js
+++ backend-stub/feedback/index.js
@@ -1,28 +1,54 @@
+const fetch = require("node-fetch");
+
 module.exports = async function (context, req) {
-  try {
-    context.log("JavaScript HTTP trigger function processed a request.");
-    // Read incoming data
-    const name = req.query.name;
-    const sport = req.query.sport; // fail if incoming data is required
-    if (!name || !sport) {
-      context.res = { status: 400 };
-      return;
-    } // Add or change code here
-    const message = `${name} likes ${sport}`;
-    // Construct response
-    const responseJSON = {
-      name: name,
-      sport: sport,
-      message: message,
-      success: true,
-    };
-    context.res = {
-      status: 200,
-      /* Defaults to 200 */
-      body: responseJSON,
-      contentType: "application/json",
-    };
-  } catch (err) {
-    context.res = { status: 500 };
-  }
+  context.log("JavaScript HTTP trigger function processed a request.");
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: JSON.stringify(req.body),
+  };
+
+  const apiCall = async (url, data) => {
+    try {
+      const config = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+        body: JSON.stringify(data),
+      };
+      const response = await fetch(url, config);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = name
+    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+  apiCall(
+    "https://prod-139.westus.logic.azure.com:443/workflows/22b04a18b2d740d9ab30bdaf56912836/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=4S70ZyQpmbMCJxFXpEWo3aEeVj4-i7oUlBe-ARQ7snc",
+    context.req.body
+  )
+    .then((data) => {
+      data.json();
+    })
+    .then((response) => context.res.json(response))
+    .catch(
+      (err) =>
+        (context.res = {
+          status: 500,
+          body: `Request error. ${err}`,
+        })
+    );
+  context.done();
 };
