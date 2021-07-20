754607acf22c1c7b715fc15fdb5e8e50f5e5713a
Kara Siegel <ksiegel1923@gmail.com>
2021-07-20 13:32:30 -0400

    added back in env variables and changed name of api call for userResearch

--- backend-stub/userResearch/index.js
+++ backend-stub/userResearch/index.js
@@ -14,10 +14,7 @@ module.exports = async function (context, req) {
     body: JSON.stringify(req.body),
   };
 
-  await fetch(
-    "https://prod-175.westus.logic.azure.com:443/workflows/f42583989bde435294b19b7b6503fc3e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VJ_vHz6TsvICCtPsL2PGhVL8rOKKpraniK9V35J7FtA",
-    config
-  )
+  await fetch(process.env.USER_RESEARCH_ENDPOINT, config)
     .then(() => (context.res = { body: "success!" }))
     .catch(
       (err) =>
