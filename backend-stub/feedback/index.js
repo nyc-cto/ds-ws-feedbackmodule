d0ebe0da5c4d2c975133bde16a493cf035f31c7a
Michelle <mliuu3259@gmail.com>
2021-07-20 11:16:37 -0400

    deleted first context.res line

--- backend-stub/feedback/index.js
+++ backend-stub/feedback/index.js
@@ -3,11 +3,6 @@ const fetch = require("node-fetch");
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: JSON.stringify(req.body),
-  };
-
   const config = {
     method: "POST",
     mode: "cors",
