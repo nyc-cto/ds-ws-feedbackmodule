4b8235ec71a47f88bcbd315edfe9f5d144056802
Kara Siegel <ksiegel1923@gmail.com>
2021-07-20 14:25:36 -0400

    removed unused lines of code in backend stub index and removed unused node package

--- backend-stub/index.js
+++ backend-stub/index.js
@@ -28,30 +28,6 @@ const apiCall = async (url, data) => {
   }
 };
 
-// When a user clicks submit, the application creates a post request to the endpoint
-app.post("/api/feedback", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "true");
-  res.header("Access-Control-Allow-Origin", "*");
-  console.log(req.body);
-  apiCall(process.env.ENDPOINT, req.body)
-    .then((data) => {
-      console.log(data);
-      res.json(data);
-    })
-    .catch((err) => console.log(err));
-});
-
-// For personally identifiable information from the user
-app.post("/api/userinfo", (req, res) => {
-  console.log(req.body);
-  apiCall(process.env.USER_RESEARCH_ENDPOINT, req.body)
-    .then((data) => {
-      console.log(data);
-      return data;
-    })
-    .catch((err) => console.log(err));
-});
-
 app.listen(process.env.PORT || 8000, () => {
   console.log("Listening on port 8000");
 });
