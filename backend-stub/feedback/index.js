85178303eba65966dd90ea48d03009b2766649b5
Michelle <mliuu3259@gmail.com>
2021-07-20 11:12:07 -0400

    updated apiCall to await

--- backend-stub/feedback/index.js
+++ backend-stub/feedback/index.js
@@ -8,40 +8,25 @@ module.exports = async function (context, req) {
     body: JSON.stringify(req.body),
   };
 
-  const apiCall = async (url, data) => {
-    try {
-      const config = {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-        body: JSON.stringify(data),
-      };
-      const response = await fetch(url, config);
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
+  const config = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify(req.body),
   };
 
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-  apiCall(
+  await fetch(
     "https://prod-139.westus.logic.azure.com:443/workflows/22b04a18b2d740d9ab30bdaf56912836/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=4S70ZyQpmbMCJxFXpEWo3aEeVj4-i7oUlBe-ARQ7snc",
-    context.req.body
+    config
   )
-    .then((data) => {
-      data.json();
-    })
+    .then((response) => response.json())
     .then((response) => context.res.json(response))
     .catch(
       (err) =>
@@ -50,5 +35,4 @@ module.exports = async function (context, req) {
           body: `Request error. ${err}`,
         })
     );
-  context.done();
 };
