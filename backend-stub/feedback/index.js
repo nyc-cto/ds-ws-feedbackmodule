8428f3d6912167c1c9b089f88ec9f42cc2d8ea58
Michelle <mliuu3259@gmail.com>
2021-07-20 10:36:14 -0400

    moved apiCall function

--- backend-stub/feedback/index.js
+++ backend-stub/feedback/index.js
@@ -1,29 +1,27 @@
-require("dotenv").config();
-
-const apiCall = async (url, data) => {
-  try {
-    const config = {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(url, config);
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
+
+  const apiCall = async (url, data) => {
+    try {
+      const config = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+        body: JSON.stringify(data),
+      };
+      const response = await fetch(url, config);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
