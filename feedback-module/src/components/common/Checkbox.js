f18a33d55c67ece3d4ea4daf5a824c38ecf4e905
Michelle <mliuu3259@gmail.com>
2021-07-22 10:29:52 -0400

    set checkbox to checked on enter

--- feedback-module/src/components/common/Checkbox.js
+++ feedback-module/src/components/common/Checkbox.js
@@ -4,6 +4,13 @@ import { Checkbox } from "@trussworks/react-uswds";
 import { CHECKBOX_STYLE } from "../../assets/styling_classnames";
 
 function ModuleCheckbox({ label, id, onCheck }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onCheck();
+      e.target.checked = !e.target.checked;
+    }
+  };
+
   return (
     <Checkbox
       id={id}
@@ -11,6 +18,7 @@ function ModuleCheckbox({ label, id, onCheck }) {
       color="primary"
       className={CHECKBOX_STYLE}
       onChange={onCheck}
+      onKeyDown={handleKeyDown}
     />
   );
 }
