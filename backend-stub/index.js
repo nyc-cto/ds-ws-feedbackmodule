3f7da500de37d82bfe4628ef6dbdc77a182c1fa8
Michelle <mliuu3259@gmail.com>
2021-07-27 15:10:32 -0400

    moved credentials to .env and referenced them in new credentials object

--- backend-stub/index.js
+++ backend-stub/index.js
@@ -5,7 +5,18 @@ const fetch = require("node-fetch");
 const cors = require("cors");
 const { google } = require("googleapis");
 const scopes = ["https://www.googleapis.com/auth/drive"];
-const credentials = require("./credentials.json");
+const credentials = {
+  type: "service_account",
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_EMAIL,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.CERT_URL,
+};
 const async = require("async");
 
 const auth = new google.auth.JWT(
