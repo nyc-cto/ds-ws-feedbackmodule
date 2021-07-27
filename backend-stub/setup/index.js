15cfad489282eba5766f3236d0bcdbcd3e208fb1
Michelle <mliuu3259@gmail.com>
2021-07-27 16:36:21 -0400

    moved credentials out of if body to test if private_key is causing issues

--- backend-stub/setup/index.js
+++ backend-stub/setup/index.js
@@ -9,20 +9,20 @@ module.exports = async function (context, req) {
   const body = req.body;
   body.id = uniqid();
 
-  if (body.method === "spreadsheet") {
-    const credentials = {
-      type: "service_account",
-      project_id: process.env.PROJECT_ID,
-      private_key_id: process.env.PRIVATE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
-      auth_uri: "https://accounts.google.com/o/oauth2/auth",
-      token_uri: "https://oauth2.googleapis.com/token",
-      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-      client_x509_cert_url: process.env.CERT_URL,
-    };
+  const credentials = {
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.CERT_URL,
+  };
 
+  if (body.method === "spreadsheet") {
     const auth = new google.auth.JWT(
       credentials.client_email,
       null,
