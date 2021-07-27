5abc20e843826d85e38aae6a255da91e8f81bb52
Michelle <mliuu3259@gmail.com>
2021-07-27 16:30:14 -0400

    moved credentials to if body

--- backend-stub/setup/index.js
+++ backend-stub/setup/index.js
@@ -2,26 +2,6 @@ const fetch = require("node-fetch");
 const uniqid = require("uniqid");
 const async = require("async");
 require("dotenv").config();
-const credentials = {
-  type: "service_account",
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.CERT_URL,
-};
-
-const auth = new google.auth.JWT(
-  credentials.client_email,
-  null,
-  credentials.private_key,
-  scopes
-);
-const drive = google.drive({ version: "v3", auth });
 
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
@@ -30,6 +10,26 @@ module.exports = async function (context, req) {
   body.id = uniqid();
 
   if (body.method === "spreadsheet") {
+    const credentials = {
+      type: "service_account",
+      project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      auth_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_uri: "https://oauth2.googleapis.com/token",
+      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+      client_x509_cert_url: process.env.CERT_URL,
+    };
+
+    const auth = new google.auth.JWT(
+      credentials.client_email,
+      null,
+      credentials.private_key,
+      scopes
+    );
+    const drive = google.drive({ version: "v3", auth });
     // splits string into emails on zero or more spaces followed by a comma and zero or more spaces
     const emails = body.emails.split(/\s*(?:,|$)\s*/);
     console.log(emails);
