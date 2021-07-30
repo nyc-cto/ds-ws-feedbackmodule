35aeb4c23afb10bbc61da9f29f6561cb2e50e42c
Michelle <mliuu3259@gmail.com>
2021-07-30 16:40:59 -0400

    moved initialize() out of renderApp so that it's only called once

--- feedback-module/src/index.js
+++ feedback-module/src/index.js
@@ -8,32 +8,24 @@ import GA4React from "ga-4-react";
 
 const WidgetDivs = document.querySelectorAll("#feedback-widget");
 
-const renderApp = (Div, lang, pagetitle, endpoint, ga4react) => {
+const renderApp = (Div, lang, pagetitle, endpoint) => {
   Div.attributes.lang && (lang = Div.attributes.lang.value);
   Div.attributes.pagetitle && (pagetitle = Div.attributes.pagetitle.value);
   Div.attributes.endpoint && (endpoint = Div.attributes.endpoint.value);
 
-  (async () => {
-    await ga4react
-      .initialize()
-      .then((res) => console.log(`Analytics Success: ${res}`))
-      .catch((err) => console.log(`Analytics Failure: ${err}`))
-      .finally(() => {
-        ReactDOM.render(
-          <React.StrictMode>
-            <Suspense fallback="... is loading">
-              <App
-                domElement={Div}
-                lang={lang}
-                pagetitle={pagetitle}
-                endpoint={endpoint}
-              />
-            </Suspense>
-          </React.StrictMode>,
-          Div
-        );
-      });
-  })();
+  ReactDOM.render(
+    <React.StrictMode>
+      <Suspense fallback="... is loading">
+        <App
+          domElement={Div}
+          lang={lang}
+          pagetitle={pagetitle}
+          endpoint={endpoint}
+        />
+      </Suspense>
+    </React.StrictMode>,
+    Div
+  );
 };
 
 WidgetDivs.forEach((Div) => {
@@ -45,19 +37,26 @@ WidgetDivs.forEach((Div) => {
   Div.attributes.gaid && (gaid = Div.attributes.gaid.value);
   const ga4react = new GA4React(gaid);
 
-  renderApp(Div, currentLang, pagetitle, endpoint, ga4react);
+  (async () => {
+    await ga4react
+      .initialize()
+      .then((res) => console.log(`Analytics Success: ${res}`))
+      .catch((err) => console.log(`Analytics Failure: ${err}`))
+      .finally(() => {
+        renderApp(Div, currentLang, pagetitle, endpoint);
+        let observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            if (mutation.type == "attributes") {
+              renderApp(Div, currentLang, pagetitle, endpoint);
+            }
+          });
+        });
 
-  let observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.type == "attributes") {
-        renderApp(Div, currentLang, pagetitle, endpoint, gaid);
-      }
-    });
-  });
-
-  observer.observe(Div, {
-    attributes: true,
-  });
+        observer.observe(Div, {
+          attributes: true,
+        });
+      });
+  })();
 });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
