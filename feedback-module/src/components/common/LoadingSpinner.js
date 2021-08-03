6a557800108ab47de67c6eb80769b300eec10d66
Kara Siegel <ksiegel1923@gmail.com>
2021-08-03 11:06:32 -0400

    tailspin loading spinner - waiting for final opinion from design team

--- feedback-module/src/components/common/LoadingSpinner.js
+++ feedback-module/src/components/common/LoadingSpinner.js
@@ -5,13 +5,15 @@ import styles from "../../styles/_export.module.scss";
 
 function LoadingSpinner() {
   return (
-    <Loader
-      type="Puff"
-      color={styles.primaryColor}
-      height={100}
-      width={100}
-      className="text-center"
-    />
+    <div>
+      <Loader
+        type="TailSpin"
+        color={styles.primaryColor}
+        height={75}
+        width={75}
+        className="text-center"
+      />
+    </div>
   );
 }
 
