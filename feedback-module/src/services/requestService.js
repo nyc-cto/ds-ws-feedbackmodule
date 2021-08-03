76c679bbbebed75e8d865f2f50918e6c9342e8b4
Kara Siegel <ksiegel1923@gmail.com>
2021-08-03 16:14:23 -0400

    cleanup before PR

--- feedback-module/src/services/requestService.js
+++ feedback-module/src/services/requestService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const requestService = (apiEndpoint, obj) => {
   axios
-    .post(`.${process.env.REACT_APP_BACKEND_ENDPOINT}/api/${apiEndpoint}`, obj)
+    .post(`${process.env.REACT_APP_BACKEND_ENDPOINT}/api/${apiEndpoint}`, obj)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
