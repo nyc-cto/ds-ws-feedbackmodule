45435eb0800cd0500fcb9dd8d41da179b4293f76
Michelle <mliuu3259@gmail.com>
2021-08-17 17:27:03 -0400

    added support for changing logo and checkmark svg colors, moved theme-aware settings to common file

--- feedback-module/src/components/common/Header.js
+++ feedback-module/src/components/common/Header.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { GridContainer } from "@trussworks/react-uswds";
 
-import Logo from "../../assets/header_logo.svg";
-
 function Header({ innerRef }) {
   return (
     <>
       {/* Using dummy empty div to host headerRef since ref does not work with GridContainer  */}
       <div ref={innerRef}></div>
       <GridContainer className="feedback-module__header">
-        <img
-          className="feedback-module__logo"
-          src={Logo}
-          alt="New York City feedback module logo"
-        />
+        <span className="feedback-module__logo"></span>
       </GridContainer>
     </>
   );
