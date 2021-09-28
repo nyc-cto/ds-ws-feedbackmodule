d3956a55f0510c143d3af44dcb579fd0f22c53f9
Rapi Castillo <racastillo@cto.nyc.gov>
2021-09-28 13:46:36 -0400

    Fix and add rate limit

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,19 @@ const express = require("express");
 const fetch = require("node-fetch");
 const uniqid = require("uniqid");
 const cors = require("cors");
-
+const rateLimit = require("express-rate-limit");
 const async = require("async");
 
 const app = express();
 app
+  .use(
+    rateLimit({
+      windowMs: 60 * 60 * 1000,
+      max: 100,
+      message: "You exceeded 100 requests per hour limit!",
+      headers: true,
+    })
+  )
   .use(cors({ origin: true, credentials: true, methods: ["POST"] }))
   .use(express.json())
   .use(express.urlencoded({ extended: true }));
